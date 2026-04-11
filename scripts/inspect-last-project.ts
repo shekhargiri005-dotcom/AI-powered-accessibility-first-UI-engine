1f1f38f3d7a279029df6a47b946dbb91c1fe1bcd
shekhargiri005-dotcom <shekhargiri005@gmail.com>
2026-04-11 13:08:48 +0530

    fix(build): remove debug script that broke TS type check

--- scripts/inspect-last-project.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const version = await prisma.projectVersion.findFirst({ orderBy: { timestamp: 'desc' } });
-  console.log(version.code);
-}
-main().catch(console.error);
